1f7664198b830440719c206178ae1e44f443f592
Fcc2187 <fcc3@cesar.school>
2025-09-11 09:29:44 -0300

    changed Tecnico em enfermagem description

--- clinica-maria-rita/src/pages/Equipe.js
+++ clinica-maria-rita/src/pages/Equipe.js
@@ -7,7 +7,7 @@ const equipeData = [
   { nome: 'Médico(a) de Família e Comunidade', especialidade: 'Atendimento médico geral' },
   { nome: 'Enfermeira(o)', especialidade: 'Consulta de enfermagem e coordenação do cuidado' },
   { nome: 'Dentista', especialidade: 'Saúde bucal e atendimento odontológico' },
-  { nome: 'Técnico(a) de Enfermagem', especialidade: 'Suporte em procedimentos e vacinação' },
+  { nome: 'Técnico(a) de Enfermagem', especialidade: 'Procedimentos e vacinação' },
   { nome: 'Técnico(a) de Saúde Bucal', especialidade: 'Assistência em odontologia' },
   { nome: 'Agentes Comunitários de Saúde', especialidade: 'Visitas domiciliares e elo com a comunidade' },
 ];
